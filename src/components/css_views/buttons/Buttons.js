dc89ddc9a6063ca352758769d218e095fae192e9
heziRahmani <hezirahmani@gmail.com>
2022-05-18 10:21:46 +0300

    .
    .

--- src/components/css_views/buttons/Buttons.js
+++ src/components/css_views/buttons/Buttons.js
@@ -186,7 +186,7 @@ export default function ButtonsUi() {
         <div className='css_btn_box btnBox5'>
           <ul>
             <li>
-              <a class='facebook' href='#'>
+              <a className='facebook' href='#'>
                 <span></span>
                 <span></span>
                 <span></span>
@@ -195,7 +195,7 @@ export default function ButtonsUi() {
               </a>
             </li>
             <li>
-              <a class='twitter' href='#'>
+              <a className='twitter' href='#'>
                 <span></span>
                 <span></span>
                 <span></span>
@@ -204,7 +204,7 @@ export default function ButtonsUi() {
               </a>
             </li>
             <li>
-              <a class='instagram' href='#'>
+              <a className='instagram' href='#'>
                 <span></span>
                 <span></span>
                 <span></span>
@@ -242,20 +242,7 @@ export default function ButtonsUi() {
           </div>
         </div>
         {/* btn8 delete animeted x*/}
-        <div className='css_btn_box btnBox8'>
-          <button className='noselect'>
-            <span className='text'>Delete</span>
-            <span className='icon'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'>
-                <path d='M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z' />
-              </svg>
-            </span>
-          </button>
-        </div>
+
         {/* btn9 glass*/}
         <div className='css_btn_box btnBox9'>
           <button className='noselect'>Glass</button>
